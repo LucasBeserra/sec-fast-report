5d8ff60ac86b8c997f61dc4e70a6b540af382c00
Lucas Beserra <lbeserra.under@gmail.com>
2025-03-22 11:08:26 -0300

    Atualizando Head da aplicação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import ActionButtons from '@/components/ActionButton';
 import QualityCheckTable from '@/app/qualityreport/QualityCheckTable';
 import MainHeader from '../components/MainHeader';
-
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'FastReport App',
+  description: 'Aplicação para gerar relatórios de qualidade',
+}
+ 
 export default function Home() {
   return (
     <div className='min-h-screen bg-gray-100 rounded-lg flex flex-col justify-content-center'>
